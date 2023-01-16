4642a0872a54fe13562a797725fd20ffabf153fc
Vinoth Rathinam <44257903+vinothrwins@users.noreply.github.com>
2023-01-16 19:25:59 +0530

    Update MultipleBrowser.java
    Updated the driver path

--- src/test/java/com/automation/sanity/MultipleBrowser.java
+++ src/test/java/com/automation/sanity/MultipleBrowser.java
@@ -19,15 +19,15 @@ public class MultipleBrowser {
 	public void launchApplication(String browser,String url) {
 		
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "\\Drivers\\chromedriver.exe");
 			driver = new ChromeDriver();
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", ".\\Drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+ "\\Drivers\\geckodriver.exe");
 			driver = new FirefoxDriver();			
 		}	
 		else if(browser.equalsIgnoreCase("edge")) {
-			System.setProperty("webdriver.edge.driver", ".\\Drivers\\msedgedriver.exe");
+			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+ "\\Drivers\\msedgedriver.exe");
 			driver = new EdgeDriver();
 		}
 		
