e4277f2e2cf20ee30479066d3a574ce18c7e2659
Vinoth Rathinam <44257903+vinothrwins@users.noreply.github.com>
2023-01-17 10:17:45 +0530

    Update MultipleBrowser.java

--- src/test/java/com/automation/sanity/MultipleBrowser.java
+++ src/test/java/com/automation/sanity/MultipleBrowser.java
@@ -19,15 +19,15 @@ public class MultipleBrowser {
 	public void launchApplication(String browser,String url) {
 		
 		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "//Drivers//chromedriver");
+			System.setProperty("webdriver.chrome.driver", ".//Drivers//chromedriver");
 			driver = new ChromeDriver();
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", ".//Drivers//geckodriver");
+			System.setProperty("webdriver.gecko.driver", "..//Drivers//geckodriver");
 			driver = new FirefoxDriver();			
 		}	
 		else if(browser.equalsIgnoreCase("edge")) {
-			System.setProperty("webdriver.edge.driver", "//Drivers///msedgedriver");
+			System.setProperty("webdriver.edge.driver", "//AzureDevOpsTestNG//Drivers///msedgedriver");
 			driver = new EdgeDriver();
 		}
 		
